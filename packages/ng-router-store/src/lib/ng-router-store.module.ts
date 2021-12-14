fa4ff4b6f4063765c8f5fa29263db6733ba7da1d
Boetzkes, Yannick <code@yannickboetzkes.com>
2021-12-14 18:27:18 +0100

    chore(router-store): format

--- packages/ng-router-store/src/lib/ng-router-store.module.ts
+++ packages/ng-router-store/src/lib/ng-router-store.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterService } from './router.service';
 
 @NgModule({
